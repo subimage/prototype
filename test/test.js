c2a9d25132cbe5006620bfbe71497630235afbbc
Jason Westbrook <jwestbrook@gmail.com>
2013-08-12 18:22:57 -0700

    make it run all the tests and hopefully spawn in a browser

--- test/test.js
+++ test/test.js
@@ -1,16 +1,26 @@
 var sys = require('sys')
 var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
 
-
-console.log('Running Tests through PhantomJS')
 describe("Running Tests through PhantomJS",function(){
-	it("",function(done){
-		function puts(error, stdout, stderr) {
+	this.timeout(0)
+	it("Base",function(done){
+		exec('mocha-phantomjs ./test/index.html',function(error, stdout, stderr) {
 			sys.puts(stdout)
 			done()
-		}
-		exec('/usr/local/bin/mocha-phantomjs ./test/index.html',puts);
+		});
 	});
+	it("DOM",function(done){
+		exec('mocha-phantomjs ./test/domtests.html',function(error, stdout, stderr) {
+			sys.puts(stdout)
+			done()
+		});
+	})
 });
 
+describe("Running Tests in deafult browser",function(){
+	it("",function(){
+		spawn('open', ['file://'+process.cwd()+'test/index.html']);
+	});
+});
 
