82779a640eff9392fb461fc0c9f3b3d868bb53bb
Jason Westbrook <jwestbrook@gmail.com>
2013-08-22 14:53:11 -0700

    Periodical Executer Test

--- test/unit/periodical_executer.test.js
+++ test/unit/periodical_executer.test.js
@@ -1,5 +1,5 @@
-new Test.Unit.Runner({
-  testPeriodicalExecuterStop: function() {
+suite("PeriodicalExecuter",function(){
+  test("PeriodicalExecuter.stop()",function(done) {
     var peEventCount = 0;
     function peEventFired(pe) {
       if (++peEventCount > 2) pe.stop();
@@ -8,28 +8,29 @@ new Test.Unit.Runner({
     // peEventFired will stop the PeriodicalExecuter after 3 callbacks
     new PeriodicalExecuter(peEventFired, 0.05);
 
-    this.wait(600, function() {
-      this.assertEqual(3, peEventCount);
-    });
-  },
+    setTimeout(function() {
+      assert.equal(3, peEventCount);
+      done()
+    },600);
+  });
 
-  testOnTimerEventMethod: function() {
+  test("OnTimer Event Method", function() {
     var testcase = this,
         pe = {
           onTimerEvent: PeriodicalExecuter.prototype.onTimerEvent,
           execute: function() {
-            testcase.assert(pe.currentlyExecuting);
+            assert(pe.currentlyExecuting);
           }
         };
 
     pe.onTimerEvent();
-    this.assert(!pe.currentlyExecuting);
+    assert(!pe.currentlyExecuting);
 
     pe.execute = function() {
-      testcase.assert(pe.currentlyExecuting);
+      assert(pe.currentlyExecuting);
       throw new Error()
     }
-    this.assertRaise('Error', pe.onTimerEvent.bind(pe));
-    this.assert(!pe.currentlyExecuting);
-  }
+    assert.throws(pe.onTimerEvent.bind(pe));
+    assert(!pe.currentlyExecuting);
+  });
 });
